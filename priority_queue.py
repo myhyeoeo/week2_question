990b977e4eef94f5df724d6c11898517057c8a04
myhyeoeo <126792624+myhyeoeo@users.noreply.github.com>
2024-03-24 14:32:59 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -3,21 +3,18 @@ class PriorityQueue:
     self.heap = []
 
   def push(self, item, priority):
-    entry = (priority, item)
+    entry = (-priority, item)
     self.heap.append(entry)
     self._sift_up(len(self.heap) - 1)
 
   def pop(self):
-    if len(self.heap) > 1:
-      self._swap(0, len(self.heap) - 1)
-      priority, item = self.heap.pop()
-      self._sift_down(0)
-      return item
-    elif len(self.heap) == 1:
-      priority, item = self.heap.pop()
-      return item
-    else:
-      return None
+    if not self.heap:
+        raise IndexError("Priority queue is empty")
+
+    self._swap(0, len(self.heap) - 1)
+    priority, item = self.heap.pop()
+    self._sift_down(0)
+    return item
 
   def _sift_up(self, index):
     while index > 0:
@@ -35,11 +32,11 @@ class PriorityQueue:
       smallest = index
 
       if left_child_index < len(self.heap) and \
-        self.heap[left_child_index][0] > self.heap[smallest][0]:
+        self.heap[left_child_index][0] < self.heap[smallest][0]:
         smallest = left_child_index
 
       if right_child_index < len(self.heap) and \
-        self.heap[right_child_index][0] > self.heap[smallest][0]:
+        self.heap[right_child_index][0] < self.heap[smallest][0]:
         smallest = right_child_index
 
       if smallest != index:
